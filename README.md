6a556b9677f07b7e5e09f9faa391f924ef707ae7
Atamert Ölçgen <muhuk@muhuk.com>
2015-05-24 15:10:01 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# pointer
+
+Points to the exact file position of forms or nearby.
+
+Pointer is ripped out of [Midje](https://github.com/marick/Midje)'s guts.
+
+License: [MIT](http://en.wikipedia.org/wiki/MIT_License)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject pointer "0.1.0-SNAPSHOT"
+  :description "Points to the exact file position of forms or nearby."
+  :url "https://github.com/marick/pointer"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :dependencies [[midje "1.7.0-beta1"]
+                 [org.clojure/clojure "1.7.0-RC1"]]
+  :plugins [[lein-midje "3.1.3"]])
